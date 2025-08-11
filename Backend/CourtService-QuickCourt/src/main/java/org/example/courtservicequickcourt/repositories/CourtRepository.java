ee7846cc5bd2704048389432e6f375a335c6c5ec
Venkatesh Rapolu <124189195+Venkatesh0768@users.noreply.github.com>
2025-08-11 23:50:15 +0530

    Court Service

--- Backend/CourtService-QuickCourt/src/main/java/org/example/courtservicequickcourt/repositories/CourtRepository.java
+++ Backend/CourtService-QuickCourt/src/main/java/org/example/courtservicequickcourt/repositories/CourtRepository.java
@@ -1,4 +1,8 @@
 package org.example.courtservicequickcourt.repositories;
 
-public class CourtRepository {
+import org.example.entityservicequickcourt.models.Court;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourtRepository extends JpaRepository<Court, String> {
+    // Additional query methods can be defined here if needed
 }
---
+++ Backend/CourtService-QuickCourt/src/test/java/org/example/courtservicequickcourt/CourtServiceQuickCourtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.courtservicequickcourt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourtServiceQuickCourtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
