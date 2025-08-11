fd9ebd7c7fdc56f935b8dc26f3aa5afdc7282c52
Venkatesh Rapolu <124189195+Venkatesh0768@users.noreply.github.com>
2025-08-12 05:14:15 +0530

    frontend

--- Backend/AuthService-QuickCourt/src/main/java/org/example/authservicequickcourt/AuthServiceQuickCourtApplication.java
+++ Backend/AuthService-QuickCourt/src/main/java/org/example/authservicequickcourt/AuthServiceQuickCourtApplication.java
@@ -3,15 +3,15 @@ package org.example.authservicequickcourt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EntityScan(basePackages = "org.example.entityservicequickcourt.models")
+@EnableDiscoveryClient
 public class AuthServiceQuickCourtApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceQuickCourtApplication.class, args);
     }
-
 }
