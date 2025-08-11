1f528ff82722f058003f77a1414b336d77bd4ea6
Venkatesh Rapolu <124189195+Venkatesh0768@users.noreply.github.com>
2025-08-11 13:40:28 +0530

    first commit

---
+++ src/main/java/org/example/quickcourtentityservice/QuickCourtEntityServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.quickcourtentityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickCourtEntityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickCourtEntityServiceApplication.class, args);
+    }
+
+}
