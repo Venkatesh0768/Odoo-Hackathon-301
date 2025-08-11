da724124e1d4ec7485b000e3c7a79b8328f42b6e
Venkatesh Rapolu <124189195+Venkatesh0768@users.noreply.github.com>
2025-08-11 17:06:11 +0530

    auth service

---
+++ Backend/AuthService-QuickCourt/src/main/java/org/example/authservicequickcourt/controllers/UserController.java
@@ -0,0 +1,11 @@
+package org.example.authservicequickcourt.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+
+}
