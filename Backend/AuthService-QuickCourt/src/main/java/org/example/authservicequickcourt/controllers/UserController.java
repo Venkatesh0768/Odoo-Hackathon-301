443c52e52b9737962bd2276e6bf6927694bf3fb4
Venkatesh Rapolu <124189195+Venkatesh0768@users.noreply.github.com>
2025-08-11 23:47:29 +0530

    Auth Service

--- Backend/AuthService-QuickCourt/src/main/java/org/example/authservicequickcourt/controllers/UserController.java
+++ Backend/AuthService-QuickCourt/src/main/java/org/example/authservicequickcourt/controllers/UserController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
+
+@CrossOrigin(origins = "http://localhost:5173" , allowCredentials = "true")
 public class UserController {
 
     private final AuthService authService;
